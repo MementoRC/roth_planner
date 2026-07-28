7a5d13b8c0428422ebae3d9a6f08e5181ad6f1e7
mementorc <claude.rc@gmail.com>
2026-07-28 11:43:03 -0500

    refactor(ytd): add event log partial function (not yet wired)

--- views/ytd_income/_partials/__init__.py
+++ views/ytd_income/_partials/__init__.py
@@ -1,3 +1,4 @@
+from ._event_log import render_event_log_partial
 from ._sync_scan import render_sync_scan_partial
 
-__all__ = ["render_sync_scan_partial"]
+__all__ = ["render_event_log_partial", "render_sync_scan_partial"]
